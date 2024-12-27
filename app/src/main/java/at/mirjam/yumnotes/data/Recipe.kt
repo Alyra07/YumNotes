a4bf6346950751e860842ee67aaeeb14fef92c50
Alyra07 <mirjam.mclachlan@gmail.com>
2024-12-27 20:34:51 +0100

    Initial commit?

---
+++ app/src/main/java/at/mirjam/yumnotes/data/Recipe.kt
@@ -0,0 +1,12 @@
+package at.mirjam.yumnotes.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recipes")
+data class Recipe(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val ingredients: String,
+    val instructions: String
+)
